f50a8e0131c86d375fcc9748b07f9bcb9dcb42e3
Shellderon <SHELDONWALOWITZ@proton.me>
2025-02-13 19:52:44 -0800

    Add files via upload

---
+++ unit4 lessons 2 zscores.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui 
+
+print(dir(selenium.webdriver.support.ui))
